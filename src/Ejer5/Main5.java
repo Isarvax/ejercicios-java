b3075bf86a1ae96269fe4b2286627a073218cb3c
Isarvae <sblandon2223@cue.edu.com>
2023-02-04 22:37:22 -0500

    first commit

---
+++ src/Ejer5/Main5.java
@@ -0,0 +1,25 @@
+package Ejer5;
+
+import Ejer4.Semaforo;
+
+public class Main5 {
+
+    public static void main(String[] args) {
+        PagoEmpleado pagoEmpleado = new PagoEmpleado();
+
+        pagoEmpleado.codEmpleado = 143434;
+        pagoEmpleado.setSalarioBasico(30000);
+        pagoEmpleado.horasExtra = 6;
+        pagoEmpleado.setNumeroHoras(48);
+        pagoEmpleado.setDescuentoPrestamo(1000000);
+
+        System.out.println("El valor de las horas extra es de: " + pagoEmpleado.calcularValorHE());
+        System.out.println("El valor del devegado es de: " + pagoEmpleado.calcularDevengado());
+        System.out.println("El valor del deducido es de: " + pagoEmpleado.calcularDeducido());
+        System.out.println("El neto de su pago es de: " + pagoEmpleado.calcularNeto());
+
+
+
+
+    }
+}
